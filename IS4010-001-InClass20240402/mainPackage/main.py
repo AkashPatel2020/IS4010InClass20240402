149ef87258c4478028f474145ec885fc5e642eae
nicholdw <nicholdw@LCBROOM3260-INX.uccob.uc.edu>
2024-04-02 09:06:30 -0400

    Added test cases in main

--- IS4010-001-InClass20240402/mainPackage/main.py
+++ IS4010-001-InClass20240402/mainPackage/main.py
@@ -1,3 +1,9 @@
 # main.py
+from classCollaborationPackage.harrints import harrints
+from classCollaborationPackage.Patel5a5 import patel5a5
+
+
 if __name__ == "__main__":
     print("In Class 2024-04-02")
+    #harrints()
+    print(patel5a5(456))
